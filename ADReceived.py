d550286ab985c7b89d308e6df82126a53c726dc1
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-03-31 20:24:07 +0900

    add timestamp

--- ADReceived.py
+++ ADReceived.py
@@ -1,6 +1,7 @@
 import serial
 import threading
 import time
+import datetime
 
 class SerialMonitor:
     def __init__(self,port,baudrate):
@@ -10,7 +11,8 @@ class SerialMonitor:
         self.closing = False
         self.sleeptime=0.00005
 
-    def start(self):
+    def start(self,datetimeadd=False):
+        self.datetimeadd=datetimeadd
         self.closing = True
         self.ser = serial.Serial(self.port,self.baudrate)
         self.th=threading.Thread(target=self._receive)
@@ -24,5 +26,10 @@ class SerialMonitor:
     def _receive(self):
         self.data=""
         while self.closing:
-            self.data+=str(self.ser.readline())
+            if self.datetimeadd:
+                timeinfo=str(datetime.datetime.now().hour)+":"+str(datetime.datetime.now().minute)+":"+str(datetime.datetime.now().second)+"."+str(datetime.datetime.now().microsecond)
+                #self.data+=str(datetime.datetime.now())+","+str(self.ser.readline())
+                self.data+=timeinfo+","+str(self.ser.readline())
+            else:
+                self.data+=str(self.ser.readline())
             time.sleep(self.sleeptime)
--- ADReceivedtest.py
+++ ADReceivedtest.py
@@ -7,11 +7,11 @@ import time
 class SerialMonitorTest(unittest.TestCase):
     def testSerial(self):
         s1 = ADReceived.SerialMonitor("/dev/tty.usbserial",38400)
-        s1.start()
+        s1.start(datetimeadd=False)
         time.sleep(1.5) 
         print(s1.data)
         s1.stop()
-        s1.start()
+        s1.start(datetimeadd=True)
         time.sleep(1.5) 
         print(s1.data)
         s1.stop()
